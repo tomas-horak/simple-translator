74a9b55026aeb2d380bdfb8be4bc43aad5359639
tomas-horak <91005016+codedestroyer3000@users.noreply.github.com>
2022-06-21 23:15:59 +0200

    updatea

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const button = document.querySelector("#button")
 const selection = document.querySelector("#selection")
 const inputFromUser = document.querySelector("#user-input")
 const translateButton = document.querySelector("#button")
+const displayTranslation = document.querySelector("#display")
 
 translateButton.addEventListener("click", event => {
     event.preventDefault()
@@ -22,12 +23,9 @@ const getLanguages = async () => {
             }
           } );
         const data = await response.json()
-        
-        
-
         data.data.languages.forEach(element => {
             
-        selection.insertAdjacentHTML("beforeend", `<option value="${element}">${element.language}</option>`)
+        selection.insertAdjacentHTML("beforeend", `<option value="${element.language}">${element.language}</option>`)
     })}catch (error){
         console.error(error)
         //přidat funkci která zobrazí chybu
@@ -39,7 +37,7 @@ const translate = async () => {
     try {
         const encodedParams = new URLSearchParams();
         encodedParams.append("q", inputFromUser.value);
-        encodedParams.append("target", "es");
+        encodedParams.append("target", selection.value);
         encodedParams.append("source", "en");
 
         const response = await fetch("https://google-translate1.p.rapidapi.com/language/translate/v2",  {
@@ -54,8 +52,11 @@ const translate = async () => {
         );
         
         const data = await response.json()
+        displayTranslation.value = data.data.translations[0].translatedText
+
+
         console.log(data.data.translations[0].translatedText)
-        console.log(selection)
+        
 
     } catch (error) {
         console.error(error)
